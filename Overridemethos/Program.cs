9b963809ee8af2dba5411f3670a4aa4927b0bc66
JiaLee0707 <s2018s37@e-mirim.hs.kr>
2020-06-02 13:27:58 +0900

    add Override methos

--- Overridemethos/Program.cs
+++ Overridemethos/Program.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,46 @@ namespace Overridemethos
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(Program.Power(2));
+            Console.WriteLine(Program.Power(2, 3));
+            Console.WriteLine(Program.SumAll(5));
+            Console.WriteLine(Program.SumAll(0, 5));
+
+        }
+
+        public static int Power(int input)
+        {
+            return input*=input;
+        }
+        public static int Power(int input, int count)
+        {
+            int power = input;
+            for(int i=0; i<count-1; i++)
+            {
+                power = power * input;
+            }
+            return power;
+        }
+
+        public static int SumAll(int end)
+        {
+            int sum = 0;
+            for(int i=0; i<end; i++)
+            {
+                sum += i;
+            }
+
+            return sum;
+        }
+        public static int SumAll(int start, int end)
+        {
+            int sum = 0;
+            for (int i = start; i < end; i++)
+            {
+                sum += i;
+            }
+
+            return sum;
         }
     }
 }
